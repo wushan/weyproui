e1cd0f9fb92f9992541ca8f616f480e82f520168
wushan <chiangloveslynn@gmail.com>
2016-07-07 13:03:20 +0800

    fix $.browser

--- .tmp/scripts/vendor/viewport.js
+++ .tmp/scripts/vendor/viewport.js
@@ -11,34 +11,14 @@ function isMobile(){
 }
 
 //Height
-//Doing this just for window detection
-jQuery.browser = {};
-    (function () {
-        jQuery.browser.msie = false;
-        jQuery.browser.version = 0;
-        if (navigator.userAgent.match(/MSIE ([0-9]+)\./)) {
-            jQuery.browser.msie = true;
-            jQuery.browser.version = RegExp.$1;
-        }
-    })();
 
 //SET VIEW PORT HEIGHT Functionally
 function getBrowserHeight() {
-  if ($.browser.msie) {
-      return document.compatMode == "CSS1Compat" ? document.documentElement.clientHeight :
-               document.body.clientHeight;
-  } else {
-      return self.innerHeight;
-  }
+  return self.innerHeight;
 }
 
 function getBrowserWidth() {
-  if ($.browser.msie) {
-    return document.compatMode == "CSS1Compat" ? document.documentElement.clientWidth :
-             document.body.clientWidth;
-  } else {
-    return self.innerWidth;
-  }
+  return self.innerWidth;
 }
 
 /*
