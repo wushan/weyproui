71fee453f5d608d5eeb9338d19ee0043e7e8052a
wushan <chiangloveslynn@gmail.com>
2016-07-07 12:54:49 +0800

    -

--- build/scripts/weypro.sticky.js
+++ build/scripts/weypro.sticky.js
@@ -270,7 +270,8 @@
 //Init Plugins
 $(document).ready(function(){
 	$(".sticker").sticky({
-		topSpacing:0
+		topSpacing:0,
+		zIndex: 99
 	});
 	$(window).resize(function(){
 		$(".sticker").sticky('update');
